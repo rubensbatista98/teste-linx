9288f839bb56626506610b5ce7a87bd7a56ef271
Rubens Batista <batistarubinho@gmail.com>
2021-01-23 15:23:20 -0300

    feat: add useFetch

---
+++ src/utils/hooks/useFetch.ts
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export const API_URL = 'https://api.openbrewerydb.org';
+
+type Status = 'pending' | 'rejected' | 'resolved';
+
+export function useFetch<TData = unknown>(endpoint: string) {
+  const [data, setData] = React.useState<TData>();
+  const [status, setStatus] = React.useState<Status>('pending');
+  const [error, setError] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    setStatus('pending');
+
+    api(`${API_URL}/${endpoint}`).then(
+      (data) => {
+        setData(data);
+        setStatus('resolved');
+      },
+      (error) => {
+        setError(error.message);
+        setStatus('rejected');
+      }
+    );
+  }, [endpoint]);
+
+  return { status, data, error };
+}
+
+async function api(url: string) {
+  try {
+    const response = await fetch(url);
+
+    const data = response.json ? await response.json() : response;
+
+    // have some errors where the API send an array as a response
+    const isAValidError = !response.ok && response.json;
+
+    if (response.ok) {
+      return data;
+    }
+
+    if (isAValidError) {
+      return Promise.reject(data);
+    }
+
+    return Promise.reject({ message: 'Something went wrong.' });
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
