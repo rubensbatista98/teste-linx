51433fa0791862b1e46a86b7fbac9cb8da43b9c8
Rubens Batista <batistarubinho@gmail.com>
2021-01-24 23:40:25 -0300

    feat: add pagination to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { CircularProgress, Grid, Typography } from '@material-ui/core';
+import { Pagination, PaginationItem } from '@material-ui/lab';
 
 import BreweryCard from 'components/BreweryCard';
 
@@ -7,8 +8,16 @@ import { Brewery } from 'entities';
 import { useFetch } from 'utils/hooks';
 
 function Home() {
-  const { data: breweries, status, error } = useFetch<Brewery[]>('breweries');
   const history = useHistory();
+  const location = useLocation();
+
+  const pageQuery = location.search
+    ? new URLSearchParams(location.search).get('page')
+    : null;
+
+  const ENDPOINT = `breweries${pageQuery ? `?page=${pageQuery}` : ''}`;
+
+  const { data: breweries, status, error } = useFetch<Brewery[]>(ENDPOINT);
 
   const isLoading = status === 'pending';
 
@@ -25,26 +34,49 @@ function Home() {
   }
 
   return (
-    <Grid
-      container
-      spacing={2}
-      component="section"
-      aria-describedby={isLoading ? '#progress' : undefined}
-      aria-busy={isLoading}
-      aria-live="polite"
-    >
-      {isLoading ? (
-        <Grid item container justify="center">
-          <CircularProgress id="progress" aria-label="loading" size={50} />
-        </Grid>
-      ) : (
-        breweries?.map((brewery) => (
-          <Grid key={brewery.id} item xs={12} sm={6} md={4} lg={3}>
-            <BreweryCard {...brewery} onClick={handleClick} />
+    <>
+      <Grid
+        container
+        spacing={2}
+        component="section"
+        justify="center"
+        aria-describedby={isLoading ? '#progress' : undefined}
+        aria-busy={isLoading}
+        aria-live="polite"
+      >
+        {isLoading ? (
+          <Grid item container justify="center">
+            <CircularProgress id="progress" aria-label="loading" size={50} />
           </Grid>
-        ))
-      )}
-    </Grid>
+        ) : (
+          <>
+            {breweries?.map((brewery) => (
+              <Grid key={brewery.id} item xs={12} sm={6} md={4} lg={3}>
+                <BreweryCard {...brewery} onClick={handleClick} />
+              </Grid>
+            ))}
+
+            <Pagination
+              page={pageQuery ? Number(pageQuery) : 1}
+              count={3}
+              hideNextButton
+              hidePrevButton
+              shape="rounded"
+              color="primary"
+              renderItem={(item) => (
+                <PaginationItem
+                  component={Link}
+                  to={`/breweries${
+                    item.page === 1 ? '' : `?page=${item.page}`
+                  }`}
+                  {...item}
+                />
+              )}
+            />
+          </>
+        )}
+      </Grid>
+    </>
   );
 }
 
