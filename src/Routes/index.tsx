90eb089d0784d623c5aac7f670a37082f9fa8dbb
Rubens Batista <batistarubinho@gmail.com>
2021-01-27 23:10:15 -0300

    feat: add Details page to Route

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,11 +1,13 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from 'pages/Home';
+import Details from 'pages/Details';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/(breweries|)" component={Home} />
+      <Route exact path="/breweries/:id" component={Details} />
       <Redirect to="/" />
     </Switch>
   );
